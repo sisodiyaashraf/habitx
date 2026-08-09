d8026e83477b229af824decd7f1baa5c6adee9ee
sisodiyaashraf <ashrafsisodiya478@gmail.com>
2026-08-09 15:26:08 +0530

    CI: Remove hardcoded ndkVersion configuration

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -7,7 +7,6 @@ plugins {
 android {
     namespace = "com.shalcontech.habitx"
     compileSdk = 36
-    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
